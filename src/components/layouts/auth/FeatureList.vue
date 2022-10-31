75ed54861c94a06b219c5c89e85953b18b7b2f56
Rio Pambudhi <riopambudhi51@gmail.com>
2022-11-01 00:34:52 +0700

    Done login and register form

---
+++ src/components/layouts/auth/FeatureList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Customizable from "@/components/layouts/auth/features/Customizable.vue";
+import Fortune from "@/components/layouts/auth/features/Fortune.vue";
+import Documentation from "@/components/layouts/auth/features/Documentation.vue";
+</script>
+<template>
+  <div class="mx-auto md:mx-0">
+    <Customizable />
+    <Fortune />
+    <Documentation />
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoryView from "../views/CategoryView.vue";
 import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,6 +20,11 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: LoginView,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
     },
   ],
 });
